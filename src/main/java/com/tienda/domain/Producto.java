875266bb1e3b10a38f907ea5dde529aff318630e
miguemesen <miguemesen12@gmail.com>
2023-10-18 21:01:42 -0600

    Semana 6 crud

---
+++ src/main/java/com/tienda/domain/Producto.java
@@ -0,0 +1,34 @@
+
+package com.tienda.domain;
+
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="producto")
+public class Producto implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_producto")
+    private Long idProducto;
+    private String descripcion;
+    private String rutaImagen;
+    private Boolean activo;
+
+    public boolean isActivo() {
+        return activo;
+    }
+    
+        public Producto() {
+    }
+
+    public Producto(String description, boolean activo) {
+        this.descripcion = descripcion;
+        this.activo = activo;
+    }
+}
---
+++ src/main/java/com/tienda/service/ProductoService.java
@@ -0,0 +1,20 @@
+package com.tienda.service;
+
+import com.tienda.domain.Producto;
+import java.util.List;
+
+public interface ProductoService {
+    // Se genera un método para obtener un ArrayList de Categorias
+    public List<Producto> getProductos(boolean activos);
+    
+   // Se obtiene un Categoria, a partir del id de un categoria
+    public Producto getProducto(Producto producto);
+    
+    // Se inserta un nuevo categoria si el id del categoria esta vacío
+    // Se actualiza un categoria si el id del categoria NO esta vacío
+    public void save(Producto producto);
+    
+    // Se elimina el categoria que tiene el id pasado por parámetro
+    public void delete(Producto producto);
+
+}
