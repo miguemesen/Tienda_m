e1d83a6511658403b71ffb48ab9b72746e0f4bc6
miguemesen <miguemesen12@gmail.com>
2023-11-29 20:44:56 -0600

    clase 12

---
+++ src/main/java/com/tienda/service/ReporteService.java
@@ -0,0 +1,18 @@
+package com.tienda.service;
+
+import java.io.IOException;
+import java.util.Map;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+
+public interface ReporteService {
+    // se define la firma del método que genera los reportes, con los siguientes parámetros
+    // 1. reporte: es el nombre del archivo de reporte (.jasper)
+    // 2. parametros: un Map que contiene los parámetros del reporte... si los ocupa
+    // 3. tipo: es el tipo de reporte: vPdf, Pdf, Xls, Csv
+    public ResponseEntity<Resource> generaReporte(
+            String reporte,
+            Map<String, Object> parametros,
+            String tipo) throws IOException;
+    
+}
